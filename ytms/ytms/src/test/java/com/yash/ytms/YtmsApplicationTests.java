3fa0f94413a6daefd36c6676548fa018990d53ce
Dheerendra  Kag <dheerendra.kag@Yash.com>
2023-05-03 11:37:58 +0530

    first commit for ytms

---
+++ ytms/ytms/src/test/java/com/yash/ytms/YtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.ytms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
