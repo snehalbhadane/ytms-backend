d9f74e19eeaa6ff1632fd687ca882564873211a9
Dheerendra  Kag <dheerendra.kag@Yash.com>
2023-06-22 16:41:01 +0530

    fix: jwtexpireexception handle

--- ytms/src/main/java/com/yash/ytms/config/JwtRequestFilter.java
+++ ytms/src/main/java/com/yash/ytms/config/JwtRequestFilter.java
@@ -10,12 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.ExpiredJwtException;
 
 /**
  * this will filter out the every coming request from client side.
@@ -40,30 +43,35 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		final String authorizationHeader = request.getHeader("Authorization");
-		System.out.println("authorizationHeader"+authorizationHeader);
-		String username = null;
-		String jwt = null;
-		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-			jwt = authorizationHeader.substring(7);
-			username = jwtUtil.extractUsername(jwt);
-		}
-		System.out.println("inside jwtRequestFilter username" + username);
-		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.userDetailService.loadUserByUsername(username);
-
-			if (jwtUtil.checkToken(jwt, userDetails)) {
-				System.out.println("inside checkToken userDetails" + userDetails);
-				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-						userDetails, null, userDetails.getAuthorities());
-				System.out.println(usernamePasswordAuthenticationToken.toString());
-				usernamePasswordAuthenticationToken
-						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-
+		try {
+			final String authorizationHeader = request.getHeader("Authorization");
+			System.out.println("authorizationHeader"+authorizationHeader);
+			String username = null;
+			String jwt = null;
+			if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+				jwt = authorizationHeader.substring(7);
+				username = jwtUtil.extractUsername(jwt);
 			}
-		}
-		filterChain.doFilter(request, response);
+			System.out.println("inside jwtRequestFilter username" + username);
+			if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+				UserDetails userDetails = this.userDetailService.loadUserByUsername(username);
+	
+				if (jwtUtil.checkToken(jwt, userDetails)) {
+					System.out.println("inside checkToken userDetails" + userDetails);
+	
+					UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+							userDetails, null, userDetails.getAuthorities());
+					System.out.println(usernamePasswordAuthenticationToken.toString());
+					usernamePasswordAuthenticationToken
+							.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+					
+				}
+			}
+			filterChain.doFilter(request, response);
+		} catch (ExpiredJwtException e) {
+            System.out.println(e.getMessage());
+	    }
 	}
 
 }
--- ytms/src/main/java/com/yash/ytms/config/JwtUtil.java
+++ ytms/src/main/java/com/yash/ytms/config/JwtUtil.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
