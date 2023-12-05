4ecdf22b8a46d0591af6774a2a111616dd7c9b05
devejavaui <devjavaui@gmail.com>
2023-12-05 12:24:37 +0530

    Remove ignored files

--- ytms/src/main/java/com/yash/ytms/exception/TRFNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.yash.ytms.exception;
-
-public class TRFNotFound extends Exception{
-    
-	public TRFNotFound(String msg) {
-		super(msg);
-	}
-}
--- ytms/src/main/java/com/yash/ytms/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.yash.ytms.exception;
-
-public class UserAlreadyExistsException extends Exception {
-
-	public UserAlreadyExistsException(String msg) {
-		super(msg);
-	}
-}
